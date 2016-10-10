d54b2b3c48b2f2dae04a5a011e58a9a12a060b60
Gloria Guy <totallygloria@gmail.com>
2016-10-10 09:47:29 -0700

    Created initial project structure following MVC and SMACSS design architecture. Most files are empty, except server and packages.

---
+++ server.js
@@ -0,0 +1,43 @@
+// Let's build a server!
+var express = require('express'),
+  proxy = require('express-request-proxy'),
+  port = process.env.PORT || 3000,
+  app = express();
+
+
+/* app.get('/github/*', function(request, response){
+  console.log('External Github request made: ', request.url);
+  // If a git hub request happens internally, glue it together here:
+  (proxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: {
+      Authorization: 'token ' + process.env.GITHUB_TOKEN
+    }
+  }))(request, response);
+});
+*/
+
+/*
+app.get('/twitter/*', function(request, response){
+  console.log('External Twitter request made: ', request.url);
+  // If a twitter request happens internally, glue it together here:
+  (proxy({
+    url: '	https://api.twitter.com/oauth/access_token' + request.params[0],
+    headers: {
+      Authorization: 'token ' + process.env.TWITTER_TOKEN
+    }
+  }))(request, response);
+});
+*/
+
+// The serve-all has to be below the specific requests or it overrides them
+app.use(express.static('./'));
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', {root: '.'});
+});
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
