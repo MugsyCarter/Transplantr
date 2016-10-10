de6a028a3310de6e97d87e39187ddf0effe501cb
Nathan Hugon <nthugon@gmail.com>
2016-10-10 13:33:53 -0700

    add select options for state/use state select option for API call to census/use census response data to populate county select options

---
+++ scripts/models/city.js
@@ -0,0 +1,41 @@
+//IIFE here
+(function(module) {
+  var census = {};
+  var censusKey = '7a3aa9d2f7fafb092b5957d10b65c477719c4c4f';
+  var stateChoice;
+//array of all site objects that
+  census.allData = [];
+
+  $('#test-button').on('click', function(){
+    census.request(alert('census.request called'));
+  });
+
+  $('#state-choice').on('change', function(){
+    stateChoice = $(this).val();
+    alert(stateChoice);
+  });
+
+//ajax call here
+  census.request = function(callback) {
+    $.ajax({
+      method: 'GET',
+      url: 'http://api.census.gov/data/timeseries/poverty/saipe?get=NAME,SAEMHI_PT,SAEPOVRTALL_PT&for=county:*&in=state:' + stateChoice + '&time=2012&key=7a3aa9d2f7fafb092b5957d10b65c477719c4c4f',
+      success: function(data, status, xhr){
+        console.log('ajax call was a rousing success:', data);
+        data.forEach(function(county){
+          if (county[0] !== 'NAME') {
+            var $option = $('<option></option>');
+            $option.text(county[0]);
+            $('#county-filter').append($option);
+            console.log(county[0]);
+          }
+        });
+      },
+      error: function(xhr, settings, error){
+        console.log('Ajax call failed:', error);
+      }
+    });
+  };
+
+  module.census = census;
+})(window);
