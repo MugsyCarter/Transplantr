e2c550bdba3a5aed00520e16f61e397264245e60
Nathan Hugon <nthugon@gmail.com>
2016-10-11 11:53:01 -0700

    rewrote proxy api request using separate function/returns xml from zillow

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -4,6 +4,4 @@ page('/', homeController.reveal);
 page('/about', aboutController.reveal);
 page('/info', infoController.reveal);
 page('/city-comparison', cityController.reveal);
-page('/zillow', MortgageData.fetchZillow);
 page();
-
