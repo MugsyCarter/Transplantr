210e7ac5d0a2369d875b1bc9022f9c51d4de3c3e
Gloria Guy <totallygloria@gmail.com>
2016-10-12 10:24:45 -0700

    In the main AJAX call to the Census data, I added into the forEach loop some logic that takes the same census data and it's looped over and turns the county, median income, and % poverty and turns them into a new Census object which is appended to a new Census.econData array. Following the general flow of the project, the array of objects are then passed to the controller, which hands them off to the view, which then generates the html using handlebars and drops it on the page.
    Data for the county is triggered when the county drop down event
    fires.

--- scripts/models/city.js
+++ scripts/models/city.js
@@ -9,6 +9,7 @@
 
   //array of all site objects that run census.
   Census.allData = [];
+  Census.economicData = [];
 
   //request when state option changes
   $('#state-choice').on('change', function(){
@@ -22,6 +23,8 @@
   $('#county-filter').on('change', function(){
     Census.countyChoice = $(this).val();
     Census.countyChoiceName = Census.countyChoice.replace(' County', '');
+    // grab the county's econ data and pass it to the controller
+    incomeController.revealEcon(Census.getEconInfo());
   });
 
   //method to find info for county
@@ -33,6 +36,23 @@
     }
   };
 
+  // method to grab just the county selected and put it's econ data on the page
+  Census.getEconInfo = function() {
+    for (var i=0; i < Census.economicData.length; i++) {
+      if (Census.economicData[i]['county'] == Census.countyChoice) {
+        var econObj = Census.economicData[i];
+        break;
+      }
+    }
+    return econObj;
+  };
+
+  // Handlebars templating to create the html
+  Census.prototype.createEconHtml = function() {
+    var template = Handlebars.compile($('#county-econ-template').html());
+    return template(this);
+  };
+
   //ajax call gets routed to the express server and then out to the census
   Census.request = function() {
     $.ajax({
@@ -40,15 +60,22 @@
       url: '/census/' + Census.stateChoice,
       success: function(data, status, xhr){
         Census.loadData(data);    // turn census data into Census objects
-        // populate the county filter with the received census data
-        data.forEach(function(county){
+        // We loop through all the data once, grabbing different things out
+        Census.allData.forEach(function(county) {
           if (county[0] !== 'NAME') {
+            // populate the county filter with the received census data
             var $option = $('<option></option>');
             $option.val(county[0]);
             $option.text(county[0]);
             $('#county-filter').append($option);
-          }
-        });
+            // grab the income and poverty data, make into a census object
+            Census.economicData.push(new Census({
+                "county": county[0],
+                "medianIncome": "$" + county[1],
+                "percentPoverty": county[2] + "%"
+              }));
+          } // close if
+        }); // close forEach
         // The rental data can't run until census populates, call it here
         RentalData.fetchStates();
       },
