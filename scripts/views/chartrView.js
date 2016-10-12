7fff249e1a4eea58170f236cfd3182688f8169ce
risafletcher <risa.fletcher@gmail.com>
2016-10-12 10:25:24 -0700

    initial styling on chart. edited alignment

--- scripts/views/chartrView.js
+++ scripts/views/chartrView.js
@@ -1,40 +1,48 @@
 'use strict';
 
 function drawChart() {
-  var nationalMedianIncome = [];
 
   var ctx = $('#chart_canvas');
 
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['National Median Income', 'Destination Median Income', 'National Median Rent', 'Destination Median Rent'],
+      labels: ['You', 'National Median Income', 'Your Median Rent', 'Destination Median Rent'],
       datasets: [{
         label: 'National Median Income',
-        data: nationalMedianIncome,
+        data: [1,25,36,4,9],
+        backgroundColor: 'rgba(0, 167, 225, 1)'
       },
       {
         label: 'Destination Median Income',
-        data: Census.allData,
+        data: [3,17,48,43,8],
+        backgroundColor: 'rgba(0, 52, 89, 1)'
       },
       {
         label: 'National Median Rent',
-        data: nationalMedianIncome,
+        data: [9,55,24,9,9],
+        backgroundColor: 'rgba(0, 126, 167, 1)'
       },
       {
         label: 'Destination Median Rent',
-        data: RentalData.stateData,
+        data: [66,33,8,44,17],
+        backgroundColor: 'rgba(0, 64, 87, 1)'
       }]
     },
     options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            stacked: true,
-            beginAtZero: true
-          }
-        }]
-      }
+      title: {
+        fontFamily: "'Open Sans', sans-serif"
+      },
+      xAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
     }
   });
 }
