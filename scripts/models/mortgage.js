f2af48773d3b2722d721c9548430a342ccbe5d4c
Gloria Guy <totallygloria@gmail.com>
2016-10-12 08:19:35 -0700

    Readded the filter code to the mortgage ajax call.

--- scripts/models/mortgage.js
+++ scripts/models/mortgage.js
@@ -28,7 +28,15 @@ To use handlebars, the data needs to be stored as an array of objects.
       url: '/zillow/' + Census.stateChoiceName.toLowerCase() + '/' + Census.countyChoiceName.toLowerCase(),
       success: function(data, status, xhr) {
         console.log('inside the fetch ajax call, data is:', data);
-      },
+        /*xmlData.forEach(function(city){
+          if (city[0] !== 'NAME') {
+            var $option = $('<option></option>');
+            $option.val(city[0]);
+            $option.text(city[0]);
+            $('#county-filter').append($option);
+          }
+        };
+      },*/
       error: function(xhr, settings, error) {
         console.log('Server returned a ', xhr.status + ' ' + error + ' error.');
       }
