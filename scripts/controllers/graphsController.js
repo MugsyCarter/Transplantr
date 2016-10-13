2dc38bde55a22925c0a441337c8ef6e893f6c002
Nathan Hugon <nthugon@gmail.com>
2016-10-13 11:39:16 -0700

    used jquery to hide and display nav with hamburger

--- scripts/controllers/graphsController.js
+++ scripts/controllers/graphsController.js
@@ -7,7 +7,7 @@
     $('.link a').css({color:'white'});
     $('#graphs-link').css({color:'grey'});
     $('#transplantr').fadeOut(function () {
-      $('#transplantr').text('City Comparisons').fadeIn();
+      $('#transplantr').hide().text('City Comparisons').fadeIn();
     });
   };
   module.graphsController = graphsController;
--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -6,8 +6,8 @@
     $('#threeColumnHome').fadeIn();
     $('.link a').css({color:'white'});
     $('#home-link').css({color:'grey'});
-    $('#transplantr').fadeOut(function() {
-      $('#transplantr').text('Transplantr: The Smarter Way to Move').fadeIn();
+    $('#transplantr').fadeOut(function () {
+      $('#transplantr').hide().text('Transplantr: The Smarter Way to Move').fadeIn();
     });
   };
 
