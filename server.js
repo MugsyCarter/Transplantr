782cd845b84e585f71e04dcdd9356742388fcfa0
Mugsy Carter <mugsycarter@gmail.com>
2016-10-13 13:40:31 -0700

    Procfile added.  config vars added.  Urls in index.html updated to https.  General fixes to make the app work on heroku.

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 var express = require('express'),
   requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
-  app = express(),
-  zillowKey = 'X1-ZWz19jfw5ars3v_1oefy',
-  censusKey = '7a3aa9d2f7fafb092b5957d10b65c477719c4c4f';
+  app = express();
 
 function proxyZillow(request, response) {
   console.log('Routing Zillow request for', request.params[0]);
@@ -13,7 +11,7 @@ function proxyZillow(request, response) {
     dataType: 'xml',
     url:'http://www.zillow.com/webservice/GetRegionChildren.htm',
     query: {
-      'zws-id': zillowKey,
+      'zws-id': process.env.zillowKey,
       state: request.params.state,
       county: request.params.county
     }
@@ -30,7 +28,7 @@ function proxyCensus(request, response) {
       'for': 'county:*',
       'in': 'state:' + request.params.state,
       time: '2012',
-      key: censusKey
+      key: process.env.censusKey
     }
   }))(request, response);
 }
