22324d122e48b0c8695275fcf1f4741e1a44c605
risafletcher <risa.fletcher@gmail.com>
2016-10-12 17:52:15 -0700

    added 2nd chart

--- scripts/views/chartrView.js
+++ scripts/views/chartrView.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var homecity = localStorage.getItem('homecity');
+var awaycity = localStorage.getItem('awaycity');
+var myIncome = parseInt(localStorage.getItem('income'));
+var homeincome = localStorage.getItem('homeincome');
+var awayincome = localStorage.getItem('awayincome');
+var homehome = localStorage.getItem('homehomePrice');
+var awayhome = localStorage.getItem('awayhomePrice');
+
 function drawChart() {
   setTimeout(function() {
 
@@ -8,42 +16,100 @@ function drawChart() {
     new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['You', 'National Median Income', 'Your Median Rent', 'Destination Median Rent'],
+        labels: ['Home Values'],
         datasets: [{
-          label: 'National Median Income',
-          data: [1,25,36,4,9],
+          label: 'You',
+          data: [myIncome],
           backgroundColor: 'rgba(0, 167, 225, 1)'
         },
         {
-          label: 'Destination Median Income',
-          data: [3,17,48,43,8],
+          label: homecity,
+          data: [homehome.slice(1)],
           backgroundColor: 'rgba(0, 52, 89, 1)'
         },
         {
-          label: 'National Median Rent',
-          data: [9,55,24,9,9],
+          label: awaycity,
+          data: [awayhome.slice(1)],
           backgroundColor: 'rgba(0, 126, 167, 1)'
         },
         {
-          label: 'Destination Median Rent',
-          data: [66,33,8,44,17],
-          backgroundColor: 'rgba(0, 64, 87, 1)'
-        }]
+          label: 'National Median',
+          data: [284000],
+          backgroundColor: 'rgba(0, 23, 31, 1)'
+        },
+      ]
       },
       options: {
-        xAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
+        scales: {
+          yAxes: [{
+            ticks: {
+              suggestedMin:0,
+              beginAtZero:true
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              suggestedMin:0,
+              beginAtZero:true
+            }
+          }]
+        }
       }
     });
   }, 300);
 };
 
 drawChart();
+
+function drawChart2() {
+  setTimeout(function() {
+
+    var ctx = $('#chart2_canvas');
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Income Comparison'],
+        datasets: [{
+          label: 'You',
+          data: [myIncome],
+          backgroundColor: 'rgba(0, 167, 225, 1)'
+        },
+        {
+          label: homecity,
+          data: [homeincome.slice(1)],
+          backgroundColor: 'rgba(0, 52, 89, 1)'
+        },
+        {
+          label: awaycity,
+          data: [awayincome.slice(1)],
+          backgroundColor: 'rgba(0, 126, 167, 1)'
+        },
+        {
+          label: 'National Median',
+          data: [51759],
+          backgroundColor: 'rgba(0, 23, 31, 1)'
+        },
+      ]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              suggestedMin:0,
+              beginAtZero:true
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              suggestedMin:0,
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
+  }, 300);
+};
+
+drawChart2();
